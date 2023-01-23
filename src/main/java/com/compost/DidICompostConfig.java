ff5f91a551825a201f56fcafe3d30b474ab3545e
Tuna <Tdb00096@gmail.com>
2023-01-22 21:08:51 -0500

    It draws a red box

--- src/main/java/com/compost/DidICompostConfig.java
+++ src/main/java/com/compost/DidICompostConfig.java
@@ -1,19 +1,19 @@
-package com.example;
+package com.compost;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface DidICompostConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "compost",
+		name = "Compost",
+		description = "compost"
 	)
 	default String greeting()
 	{
-		return "Hello";
+		return "compost";
 	}
 }
--- src/test/java/com/compost/DidICompostPluginTest.java
+++ src/test/java/com/compost/DidICompostPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.compost;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DidICompostPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DidICompostPlugin.class);
 		RuneLite.main(args);
 	}
 }
